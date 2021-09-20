4a016e5371c0cd727f5e0442ceef5ba6ac7194af
Tyler Long <tyler4long@gmail.com>
2021-09-20 16:30:41 -0700

    List window title

--- list-all-windows.js
+++ list-all-windows.js
@@ -1,9 +1,10 @@
 const systemEvents = Application('System Events');
 systemEvents.processes();
-const names = [];
 for(const process of systemEvents.processes()) {
   if(process.backgroundOnly() === false) {
-    names.push(process.name());
+    console.log('process name', process.name());
+    for(const window of process.windows()) {
+      console.log('window title', window.title());
+    }
   }
 }
-console.log(names);
