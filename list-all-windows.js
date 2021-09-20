cec8ea1e039c171e496bf420503cdcde7fc97231
Tyler Long <tyler4long@gmail.com>
2021-09-20 16:37:56 -0700

    Get window position and location

--- list-all-windows.js
+++ list-all-windows.js
@@ -3,8 +3,11 @@ systemEvents.processes();
 for(const process of systemEvents.processes()) {
   if(process.backgroundOnly() === false) {
     console.log('process name', process.name());
+    console.log('process visible', process.visible());
     for(const window of process.windows()) {
-      console.log('window title', window.title());
+      console.log('  window title', window.title());
+      console.log('    window position', window.position());
+      console.log('    window size', window.size());
     }
   }
 }
