a714eeb6e453be376bec7038fff352850e7ccd1e
Tyler Long <tyler4long@gmail.com>
2021-09-20 16:19:17 -0700

    List foreground processes

--- list-all-windows.js
+++ list-all-windows.js
@@ -2,6 +2,8 @@ const systemEvents = Application('System Events');
 systemEvents.processes();
 const names = [];
 for(const process of systemEvents.processes()) {
-  names.push(process.name());
+  if(process.backgroundOnly() === false) {
+    names.push(process.name());
+  }
 }
 console.log(names);
