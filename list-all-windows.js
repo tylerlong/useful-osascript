2f769815c558e77ca97793d785cdd4ba9dc1ac35
Tyler Long <tyler4long@gmail.com>
2021-09-20 16:52:50 -0700

    Print more props

--- list-all-windows.js
+++ list-all-windows.js
@@ -8,6 +8,13 @@ for(const process of systemEvents.processes()) {
       console.log('  window title', window.title());
       console.log('    window position', window.position());
       console.log('    window size', window.size());
+      console.log('    window focused', window.focused());
+      console.log('    window selected', window.selected());
+      console.log('    window enabled', window.enabled());
+      console.log('    window value', window.value());
+      if(window.focused()) {
+        console.log('***************')
+      }
     }
   }
 }
